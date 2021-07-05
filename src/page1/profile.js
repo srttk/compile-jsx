8800c15058586df1435f4f0cf5547f6c5b383ec5
Sarath <sarath.tk@icloud.com>
2021-07-05 17:42:02 +0530

    compile all files

---
+++ src/page1/profile.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Profile = () => {
+  return (
+    <div>
+      <h1>Sarath</h1>
+      <h2>Hello Universe</h2>
+    </div>
+  );
+};
+
+Profile.propTypes = {};
+
+export default Profile;

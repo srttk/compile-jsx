23aeef2b83068f751533be300646092597dae27b
Sarath <sarath.tk@icloud.com>
2021-07-05 16:26:04 +0530

    initial commit :fire:

---
+++ src/page1/app.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { render } from "react-dom";
+
+render(<div>Hello Page 1 app</div>, document.querySelector("body"));
---
+++ src/page2/app.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { render } from "react-dom";
+
+render(<div>Hello Page 2 app</div>, document.querySelector("body"));
